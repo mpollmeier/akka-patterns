e28c2234109c9041451b0b6124711a9b0f37d1bb
Doug Kirk <doug@wieck.com>
2014-05-21 15:04:40 -0500

    Provide manifest to get rid of erasure warning

--- src/main/scala/akkapatterns/WorkPullingPattern.scala
+++ src/main/scala/akkapatterns/WorkPullingPattern.scala
@@ -60,7 +60,7 @@ class Master[T] extends Actor {
   }
 }
 
-abstract class Worker[T: ClassTag](val master: ActorRef) extends Actor {
+abstract class Worker[T: ClassTag](val master: ActorRef)(implicit manifest: Manifest[T]) extends Actor {
   implicit val ec = context.dispatcher
 
   override def preStart {
